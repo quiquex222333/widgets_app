636273a6c44b380de11f5b78e5e398782a83d09a
Enrique Rivera <juane222333@gmail.com>
2024-08-28 20:09:49 -0400

    Adding Screens and update exports file

---
+++ lib/presentation/screens/ui_controls/ui_controls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UIControlsScreen extends StatelessWidget {
+  static const String name = 'ui_controls_screen';
+
+  const UIControlsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
