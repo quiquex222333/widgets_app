8f739973a081c26aaf951679e390aa4efeb7f71c
Enrique Rivera <juane222333@gmail.com>
2024-09-02 14:40:23 -0400

    Adding riverpod, and statenotifiers

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -12,16 +12,33 @@ const colorList = <Color>[
 
 class AppTheme {
   final int selectedColor;
+  final bool isDarkMode;
 
-  AppTheme({this.selectedColor = 0})
-      : assert(selectedColor >= 0, "Selected color must be greater than 0"),
-        assert(selectedColor < colorList.length, "Selected color must be less or equal than ${colorList.length}");
+  AppTheme({
+    this.selectedColor = 0,
+    this.isDarkMode = false,
+  })  : assert(
+          selectedColor >= 0,
+          "Selected color must be greater than 0",
+        ),
+        assert(
+          selectedColor < colorList.length,
+          "Selected color must be less or equal than ${colorList.length}",
+        );
 
   ThemeData getTheme() => ThemeData(
-    useMaterial3: true,
-    colorSchemeSeed: colorList[ selectedColor ],
-    appBarTheme: const AppBarTheme(
-      centerTitle: false
-    ),
-  );
+        useMaterial3: true,
+        brightness: isDarkMode ? Brightness.dark : Brightness.light,
+        colorSchemeSeed: colorList[selectedColor],
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
+
+  AppTheme copyWith({
+    int? selectedColor,
+    bool? isDarkMode,
+  }) =>
+      AppTheme(
+        selectedColor: selectedColor ?? this.selectedColor,
+        isDarkMode: isDarkMode ?? this.isDarkMode,
+      );
 }
